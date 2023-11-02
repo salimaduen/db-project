5a0bc679472044a9ff957807e1746f27ca27e452
salimaduen <salimaduen@hotmail.com>
2023-11-02 12:51:13 -0400

    basic implementation for product display

--- controllers/productController.js
+++ controllers/productController.js
@@ -7,8 +7,16 @@ class ProductController {
             // TODO update so there's a MAX. per page
             const products = await Product.getAllProducts();
 
+            let columns = 0;
+
+            if ( (products.length % 4) === 0) {
+                columns = products.length / 4;
+            } else {
+                columns = Math.floor((products.length / 4)) + (products.length % 4);
+            }
+            console.log(typeof(columns));
             // Send JSON of products TODO: render them to the page
-            res.render('products', { products: products });
+            res.render('products', { products: products, columns: columns });
         } catch (error) {
             console.error(error);
             res.status(500);
--- models/product/product.model.sql
+++ models/product/product.model.sql
@@ -1,9 +1,11 @@
-CREATE TABLE Product (
+CREATE TABLE IF NOT EXISTS Product (
     ProductID INT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
     Description TEXT,
     Price DECIMAL(10, 2) NOT NULL,
+    Slug VARCHAR(255) NOT NULL,
     StockQuantity INT NOT NULL,
     CategoryID INT,
     FOREIGN KEY (CategoryID) REFERENCES Category(CategoryID)
+    CONSTRAINT SLUG_UNIQUE UNIQUE (Slug)
 );
