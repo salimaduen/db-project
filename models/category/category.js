d5527bb8616ffd7c99b7e43f494e6d412b45d57b
salimaduen <salimaduen@hotmail.com>
2023-11-25 14:32:06 -0500

    Added save category feauture

--- models/category/category.js
+++ models/category/category.js
@@ -1,16 +1,37 @@
+import storeDB from '../../database/database.js';
+
 class Category {
+    static tableName = 'Category';
+
+    /**
+     * 
+     * @param {int} id 
+     * @param {String} name 
+     * @param {String} description 
+     */
+    constructor(id, name, description = null) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 
     static async getAllCategories() {
         // get all available categories
     }
 
-    /**
-     * Adds a category to the database
-     * @param {string} category 
-     * @param {string} description 
-     */
-    static async addCategory(category, description="") {
-        // add a new category
+
+    // save category to database;
+    async save() {
+        const conn = await storeDB.getConnection();
+        const query = `INSERT INTO ${Category.tableName}(CategoryID, Name, Description)
+                       VALUES (?, ?, ?)`;
+        try {
+            conn.query(query, [this.id, this.name, this.description]);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            if (conn) await conn.release();
+        }
     }
 
     static async removeCategory() {
